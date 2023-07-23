861f35c3519c770c001cf018895fdea2657a9bf0
EgbuleNuel <egbulemmanuel@gmail.com>
2023-07-23 16:03:40 +0100

    added a new component - to be completed

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -3,7 +3,14 @@ import React from "react";
 function TimeAndLocation() {
   return (
     <div>
-      <div className="flex items-center justify-center my-6"></div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Sunday, 23 July 2023 | Local time: 2:23 PM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Magboro, Ogun</p>
+      </div>
     </div>
   );
 }
